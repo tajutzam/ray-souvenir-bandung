ec09389470f208704153bdefdee4dea042caf27f
tajutzam <mohammadtajutzamzami07@gmail.com>
2025-12-26 08:22:43 +0700

    fix: favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,12 +34,13 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+    <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/') }}/assets/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/') }}/assets/img/favicon-16x16.png">
-    <link rel="manifest" href="{{ asset('/') }}/assets/img/site.webmanifest">
-
 
     <!-- CSS here -->
     <link rel="stylesheet" href="{{ asset('/') }}assets/css/bootstrap.min.css">
